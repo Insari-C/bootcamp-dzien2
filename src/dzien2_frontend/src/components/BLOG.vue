263b8f02aec97f8059421b2db51cd9a221f792c8
Kacper <newuser@DESKTOP-7F09I1K>
2024-06-26 19:30:23 +0200

    added style and delete

--- src/dzien2_frontend/src/components/BLOG.vue
+++ src/dzien2_frontend/src/components/BLOG.vue
@@ -1,37 +1,42 @@
 <template>
     <div>
-        <h2 class="text-blue-600"></h2>
-        <button @click="Pobierz Wpisy">refresh</button>
-        <div v-for="wpis in wpisy">
-                siema blog!
-        {{ wpisy }}
+        <h2 class="text-blue-600">Wpisy na bloga:</h2>
+        <div class="w-100 flex flex-row-reverse">
+            <button @click="pobierzWpisy" class="bg-blue-600 rounded-sn text-white p-4">refresh</button>
         </div>
-        <input> v-model="nowyBlog" type="text">
-        <button @click="dodajwpisy">dodaj</button>
-        {{ nowyBlog }}
+        <div class="grid mx-6 gap-4 my-4">
+            <div v-for="wpis in wpisy" class="drop-shadow-xl bg-stone-300 p-4">
+                <p>{{ wpis }}</p>
+            </div>
+        </div>
+        <div class="flex justify-center flex-col">
+            <input v-model="nowyBlog" type="text" class="border-2 border-blue-600 p-4">
+            <button @click="dodajWpisy" class="bg-blue-600 rounded-sn text-white p-4">dodaj</button>
+        </div>
     </div>
 </template>
 
 <script>
-import {dzien2_backend } from 'declarations/dzien2_backend/index'
+import { dzien2_backend } from 'declarations/dzien2_backend/index';
 
 export default {
     data() {
-        return{
-            wpisy: []
+        return {
+            wpisy: [],
+            nowyBlog: ""
         }
     },
     methods: {
-        async dodajWpisy(){
-           await dzien2_backend.dodaj_Wpisy(nowyBlog);
+        async dodajWpisy() {
+            await dzien2_backend.dodaj_wpis(this.nowyBlog);
         },
         async pobierzWpisy() {
-           this.wpisy = await dzien2_backend.odczytaj_wpisy()
-
+            this.wpisy = await dzien2_backend.odczytaj_wpisy();
         }
     },
     async mounted() {
-  this.pobierzWpisy();
+        this.pobierzWpisy();
+    }
+
 }
-}
-</script>
+</script>d
