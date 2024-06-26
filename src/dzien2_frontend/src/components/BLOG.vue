223947141f3feef2c2c2905a4841f9c5fabcc45c
Kacper <newuser@DESKTOP-7F09I1K>
2024-06-26 17:37:42 +0200

    added wpisy

--- src/dzien2_frontend/src/components/BLOG.vue
+++ src/dzien2_frontend/src/components/BLOG.vue
@@ -4,6 +4,7 @@
         siema blog!
         {{ wpisy }}
         <input v-model="nowyBlog" type="text">
+        <button @click="dodajwpisy">dodaj</button>
         {{ nowyBlog }}
     </div>
 </template>
@@ -11,7 +12,7 @@
 <script>
 import {dzien2_backend } from 'declarations/dzien2_backend/index'
 
-export|default {
+export default {
     data() {
         return{
             wpisy: []
@@ -19,12 +20,12 @@ export|default {
     },
     methods: {
         async dodajWpisy(){
-        |   await dzien2_backend.dodaj_Wpisy(nowyBlog);
-        }
-        pobierzWpisy() {
+           await dzien2_backend.dodaj_Wpisy(nowyBlog);
+        },
+        async pobierzWpisy() {
            this.wpisy = await dzien2_backend.odczytaj_wpisy()
 
         }
-    }
+    },
 }
 </script>
